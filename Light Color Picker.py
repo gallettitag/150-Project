a1778faf701adb7e26385be5f5e9947d32f031d4
gallettitag <gallettitag@gmail.com>
2022-03-29 16:25:36 -0400

    Initial commit

--- Light Color Picker.py
+++ Light Color Picker.py
@@ -1,6 +1,23 @@
-import apiGetter
+import time
+
+import Temp_Getter
 
 def pick_color():
-    current_temp = apiGetter.get_temp()
+    current_temp = Temp_Getter.get_temp()
+    color = ''
 
+    if current_temp < 40:
+        color = '0000CC'
+    elif current_temp < 65:
+        color = '#82eaff'
+    elif current_temp > 80:
+        color = '#ff8352'
+    else:
+        color = '#ff2626'
 
+    print(color)
+    return color
+
+while True:
+    pick_color()
+    time.sleep(5)
--- Temp_Getter.py
+++ Temp_Getter.py
@@ -5,13 +5,17 @@ lat = 39.4833
 long = 87.3241
 request = f"https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={long}&appid={apiId}"
 
-
-def get_temp():
+def get_requests():
     response = requests.get(request)
     json_data = response.json()
 
-    current_temp_in_k = json_data['main']['temp']
+    return json_data
 
+def get_conditions():
+    return get_requests()['weather'][0]['description']
+
+def get_temp():
+    current_temp_in_k = get_requests()['main']['temp']
     current_temp = k_to_f(current_temp_in_k)
     return current_temp
 
